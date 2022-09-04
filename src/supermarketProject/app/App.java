209523c287c15444bd9abd69d5ad0bb4ec43095c
mlutegar <mlutegar@gmail.com>
2022-09-04 19:34:21 -0300

    first commit

---
+++ src/supermarketProject/app/App.java
@@ -0,0 +1,4 @@
+package supermarketProject.app;
+
+public class App {
+}
---
+++ src/supermarketProject/model/Product.java
@@ -0,0 +1,8 @@
+package supermarketProject.model;
+
+public class Product {
+    public String name;
+    public int quantity;
+    public float price;
+    private String type;
+}
