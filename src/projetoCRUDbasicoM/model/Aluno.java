41896d010b92daeaee9cfa0b5fb9cc07fd78b497
mlutegar <mlutegar@gmail.com>
2022-09-04 19:40:21 -0300

    feat: adding methods in Aluno class

--- src/projetoCRUDbasicoM/model/Aluno.java
+++ src/projetoCRUDbasicoM/model/Aluno.java
@@ -5,4 +5,12 @@ public class Aluno {
     private String email;
     protected int matricula;
     String sexo;
+
+    public void assistirAula(){
+        System.out.println("O aluno está assistindo a aula de Java");
+    }
+
+    public String efetuarPergunta(){
+        return "O aluno está fazendo uma pergunta sobre duvida dele de Java";
+    }
 }
