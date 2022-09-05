77c412592f4467228842768cdac50f87dc3a0a4d
mlutegar <mlutegar@gmail.com>
2022-09-04 23:12:28 -0300

    feat: adding attributes and methods in Product.java

--- src/supermarketProject/app/App.java
+++ src/supermarketProject/app/App.java
@@ -1,4 +1,19 @@
 package supermarketProject.app;
 
+import supermarketProject.model.Product;
+
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) {
+        Product pro;
+        pro = new Product();
+        
+        pro.setName(JOptionPane.showInputDialog(null, "Enter the product name: ", "Input Nome", JOptionPane.QUESTION_MESSAGE));
+        pro.setPrice(Float.parseFloat(JOptionPane.showInputDialog(null, "Enter the product price: ", "Input Price", JOptionPane.QUESTION_MESSAGE)));
+        pro.setType(JOptionPane.showInputDialog(null, "Enter the product type: ", "Input Type", JOptionPane.QUESTION_MESSAGE));
+        pro.setProvider(JOptionPane.showInputDialog(null, "Enter the product provider: ", "Input Provider", JOptionPane.QUESTION_MESSAGE));
+        pro.setQuantity(Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the product quantity: ", "Input Quantity", JOptionPane.QUESTION_MESSAGE)));
+        
+    }
 }
--- src/supermarketProject/model/Product.java
+++ src/supermarketProject/model/Product.java
@@ -1,8 +1,60 @@
 package supermarketProject.model;
 
+import java.util.Date;
+
 public class Product {
-    public String name;
-    public int quantity;
-    public float price;
+    private String name;
+    private int quantity;
+    private float price;
     private String type;
+    private String provider;
+    private Date register;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setRegister(Date register) {
+        this.register = register;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Date getRegister() {
+        return register;
+    }
 }
