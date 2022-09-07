3e07b2a374dc9b0df5fad2b0c536ff9f63a3ce34
mlutegar <mlutegar@gmail.com>
2022-09-07 11:36:49 -0300

    feat: adding for each structure

--- src/projetoCRUDbasicoM/structures/EstruturaDeRepeticao.java
+++ src/projetoCRUDbasicoM/structures/EstruturaDeRepeticao.java
@@ -1,6 +1,7 @@
 package projetoCRUDbasicoM.structures;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,10 @@ public class EstruturaDeRepeticao {
     public void estruturaFor(List<String> nomes){
         for(int i = 0; i < nomes.size(); i++){
             System.out.print(nomes.get(i));
+
+            if(nomes.get(i).startsWith("Lu")) {
+                System.out.print(" (O nome começa com Lu) ");
+            }
 
             if(i != (nomes.size() - 1))
                 System.out.print(", ");
@@ -44,11 +49,15 @@ public class EstruturaDeRepeticao {
         } while(!teclado.next().equals("Senha123@"));
 
         teclado.close();
-        System.out.println("\nQuantidades de tentativas: " + contador);
+        System.out.println("\nQuantidades de tentativas: " + contador + "\n");
     }
 
     public void estruturaForEach(List<Integer> numeros){
+        Collections.sort(numeros); // ordena os números
 
+        for(int numero: numeros){ // numero é a variavél, que no for normal é o 'i'. Ele vai incrementando-se 1 em 1. // numeros é a array especificiada na mais
+            System.out.print(numero + " ");
+        }
     }
 
     public static void main(String[] args) {
@@ -56,6 +65,7 @@ public class EstruturaDeRepeticao {
         edr.estruturaFor(Arrays.asList("Luis", "Carlos Eduardo", "Antonio", "Carlos Rodrigo", "Ighor", "Lucas", "Thawan", "Thiago", "Thaisa"));
         edr.estruturaWhile(false);
         edr.estruturaDoWhile();
+        edr.estruturaForEach(Arrays.asList(532, 231, 544, 90, 43, 56, 17, 69, 21, 12, 534));
     }
 
 }
