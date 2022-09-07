d42a22190296d1c1aa2ff3656db0f65b5a40696f
mlutegar <mlutegar@gmail.com>
2022-09-07 13:18:20 -0300

    feat: adding enum class

--- src/projetoCRUDbasicoM/persist/Bolo.java
+++ src/projetoCRUDbasicoM/persist/Bolo.java
@@ -6,9 +6,7 @@ public class Bolo {
     private int qtdOleo;
     private int qtdLeite;
     private int qtdAcucar;
-    public boolean chocolate;
-    public boolean laranja;
-    public boolean doceLeite;
+    public TipoBolo tipoBolo;
 
     public Bolo(int qtdFarinha, int qtdAcucar, int qtdOleo, int qtdOvos, int qtdLeite){
         this.qtdOvos = qtdOvos;
@@ -18,9 +16,21 @@ public class Bolo {
         this.qtdLeite = qtdLeite;
     }
 
-    public static void main(String[] args) {
-        var bolo = new Bolo(300, 233, 12, 32, 43);
-        bolo.chocolate = true;
+    public Bolo(int qtdFarinha, int qtdAcucar, int qtdOleo, int qtdOvos, int qtdLeite, TipoBolo tipoBolo){
+        this.qtdOvos = qtdOvos;
+        this.qtdFarinha = qtdFarinha;
+        this.qtdAcucar = qtdAcucar;
+        this.qtdOleo = qtdOleo;
+        this.qtdLeite = qtdLeite;
+        this.tipoBolo = tipoBolo;
     }
 
+    public static void main(String[] args) {
+        var bolo = new Bolo(300, 233, 12, 32, 43);
+        bolo.tipoBolo = TipoBolo.COMUM;
+
+        var bolo2 = new Bolo(300, 233, 12, 32, 43, TipoBolo.CHOCOLATE);
+        var bolo3 = new Bolo(300, 233, 12, 32, 43, TipoBolo.LARANJA);
+        var bolo4 = new Bolo(300, 233, 12, 32, 43, TipoBolo.DOCE_DE_LEITE);
+    }
 }
---
+++ src/projetoCRUDbasicoM/persist/TipoBolo.java
@@ -0,0 +1,8 @@
+package projetoCRUDbasicoM.persist;
+
+public enum TipoBolo { //Enum é a mesma coisa que (int, float, boolean, ...)
+    COMUM,
+    CHOCOLATE,
+    LARANJA,
+    DOCE_DE_LEITE
+}
